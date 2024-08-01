6f8fa6b134b2ba330b533b4b38faf8e522fbccab
vantuyen89 <vantuyenfpl@gmail.com>
2024-08-01 19:08:13 +0700

    v1

--- client/src/config/instance.ts
+++ client/src/config/instance.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
+  withCredentials: true,
   baseURL: process.env.SERVER_URL,
   timeout: 1000,
   headers: { 'X-Custom-Header': 'foobar' }
